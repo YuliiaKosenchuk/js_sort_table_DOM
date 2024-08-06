9968083effbd07e715c3b787aa695df7f75f6b36
Yuliia Kosenchuk <yuliia.kosenchuk@gmail.com>
2024-08-06 09:14:23 +0200

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,23 @@
+"use strict";
+const table = document.querySelector("table");
+const th = table.querySelectorAll("th");
+const tBody = table.querySelector("tbody");
+th.forEach((header, index)=>{
+    header.addEventListener("click", function() {
+        const rows = [
+            ...tBody.rows
+        ];
+        const sortedRows = rows.sort((a, b)=>{
+            const aText = a.cells[index].textContent.trim();
+            const bText = b.cells[index].textContent.trim();
+            if (aText.includes("$")) {
+                const aValue = parseFloat(aText.replace(/[$,]/g, ""));
+                const bValue = parseFloat(bText.replace(/[$,]/g, ""));
+                return aValue - bValue;
+            } else return aText > bText ? 1 : aText < bText ? -1 : 0;
+        });
+        tBody.append(...sortedRows);
+    });
+});
+
+//# sourceMappingURL=index.f75de5e1.js.map
